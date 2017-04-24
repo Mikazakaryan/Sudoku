79d720467188d5313896e09686b0dc364f627b68
Mika Zakaryan <mikazakaryan94@gmail.com>
2017-04-24 03:28:32 -0700

    full changed style

--- UI/mainwindow.cpp
+++ UI/mainwindow.cpp
@@ -1,21 +1,27 @@
 #include "mainwindow.h"
-
+#include <QLineEdit>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent)
 {
-    this->setFixedSize(650,400);
+    this->setFixedSize(1700,1000);
 
-    setCentralWidget(m_window);
+    m_slot_label->setFixedSize(0,0);
 
-    m_wellcome_label->setText("Wellcome to ");
+    m_all_layout->addLayout(m_menu_layout);
+    m_all_layout->addWidget(m_slot_label);
+
+    m_wellcome_label->setText("Wellcome to");
     m_URL_label->setOpenExternalLinks(true);
     m_URL_label->setText("<a href='https://github.com/Mikazakaryan/Sudoku' style=\"color: black;\">MSudoku</a>");
 
     m_wellcome_layout->addWidget(m_wellcome_label);
     m_wellcome_layout->addWidget(m_URL_label);
 
-    m_wellcome_layout->setAlignment(Qt::AlignCenter);
+    m_wellcome_label->setAlignment(Qt::AlignBottom);
+    m_URL_label->setAlignment(Qt::AlignHCenter);
+
+    m_wellcome_layout->setAlignment(Qt::AlignHCenter);
 
     m_play->setText("Play");
     m_rules->setText("Rules");
@@ -27,10 +33,10 @@ MainWindow::MainWindow(QWidget *parent) :
     m_buttos_layout->addWidget(m_about_me);
     m_buttos_layout->addWidget(m_exit);
 
-    m_layout->addLayout(m_wellcome_layout);
-    m_layout->addLayout(m_buttos_layout);
+    m_menu_layout->addLayout(m_wellcome_layout);
+    m_menu_layout->addLayout(m_buttos_layout);
 
-    m_window->setLayout(m_layout);
+    m_window->setLayout(m_all_layout);
 
     m_font.setPointSize(20);
 
@@ -53,61 +59,53 @@ MainWindow::MainWindow(QWidget *parent) :
     m_rules->setStyleSheet(m_color);
     m_about_me->setStyleSheet(m_color);
     m_exit->setStyleSheet(m_color);
-
     m_wellcome_label->setStyleSheet(m_color);
+
     m_color = "background-color: gray";
     m_play->setStyleSheet(m_color);
     m_rules->setStyleSheet(m_color);
     m_about_me->setStyleSheet(m_color);
     m_exit->setStyleSheet(m_color);
 
+    setCentralWidget(m_window);
+
     connect(m_play, SIGNAL(clicked()), this, SLOT(play_slot()));
-    connect(m_rules, SIGNAL(clicked(bool)), this, SLOT(rule_slot()));
-    connect(m_about_me, SIGNAL(clicked(bool)), this, SLOT(about_slot()));
+    connect(m_rules, SIGNAL(clicked()), this, SLOT(rule_slot()));
+    connect(m_about_me, SIGNAL(clicked()), this, SLOT(about_slot()));
     connect(m_exit, SIGNAL(clicked()), this, SLOT(exit_slot()));
-    connect(m_back_button, SIGNAL(clicked()), this, SLOT(back_slot()));
-}
-
-void MainWindow::back_slot(){
-
 }
 
 void MainWindow::play_slot(){
-
-}
-
-void MainWindow::about_slot(){
-    m_back_button->setText("Back");
-    m_text_label->setText("I'm a Bachelor's degree student\n"
-                          "who want to get max point\n"
-                          "for this work");
-    m_font.setPointSize(25);
-    m_text_label->setFont(m_font);
-    m_text_label->setAlignment(Qt::AlignCenter);
-
-    m_text_label->setFixedSize(650,300);
+    m_slot_label->clear();
+    m_slot_label->setFixedSize(900,1000);
 
     m_font.setPointSize(20);
-    m_font.setBold(false);
-    m_back_button->setFont(m_font);
-
-    m_color = "color: black";
-    m_back_button->setStyleSheet(m_color);
+    m_check_button->setFont(m_font);
     m_color = "background-color: gray";
-    m_back_button->setStyleSheet(m_color);
+    m_check_button->setStyleSheet(m_color);
 
-    m_about_layout->addWidget(m_back_button);
-    m_about_layout->addWidget(m_text_label);
-    m_about_layout->addWidget(m_exit);
+    m_check_button->setText("Check");
 
-    m_about_window->setLayout(m_about_layout);
+    m_check_button->setFixedWidth(900);
 
-    setCentralWidget(m_about_window);
+    for (int i = 0; i < 9; ++i) {
+        for (int j = 0; j < 9; ++j) {
+            if(true){
+                m_board_grid->addWidget(new QLabel(QString::number(9*i+j)),i,j);
+            }else{
+
+            }
+        }
+    }
+
+    m_play_slot_layout->addLayout(m_board_grid);
+    m_play_slot_layout->addWidget(m_check_button);
+
+    m_slot_label->setLayout(m_play_slot_layout);
 }
 
 void MainWindow::rule_slot(){
-    m_back_button->setText("Back");
-    m_text_label->setText("A standard Sudoku puzzle consists of a grid of 9 blocks.\n"
+    m_slot_label->setText("A standard Sudoku puzzle consists of a grid of 9 blocks.\n"
                         "Each block contains 9 boxes \n"
                         "arranged in 3 rows and 3 columns."
                         "\n· There is only one valid solution\n"
@@ -118,36 +116,31 @@ void MainWindow::rule_slot(){
                         "· When you start a game of Sudoku,\n"
                         "some blocks will be pre-filled for you.\n"
                         "You cannot change these numbers in the course of the game.\n"
-                        "· Each column must contain all of the numbers 1 through 9 an\n"
+                        "· Each column must contain all of the numbers 1 through 9 and\n"
                         "no two numbers in the same column of a Sudoku puzzle can be the same.\n"
                         "· Each row must contain all of the numbers 1 through 9 and\n"
                         "no two numbers in the same row of a Sudoku puzzle can be the same.\n"
                         "· Each block must contain all of the numbers 1 through 9 and\n"
                         "no two numbers in the same block of a Sudoku puzzle can be the same.");
-    m_font.setPointSize(10);
-    m_text_label->setFont(m_font);
-    m_text_label->setAlignment(Qt::AlignCenter);
 
-    m_text_label->setFixedSize(650,300);
+    m_font.setPointSize(15);
+    m_slot_label->setFont(m_font);
 
-    m_font.setPointSize(20);
-    m_font.setBold(false);
-    m_back_button->setFont(m_font);
+    m_slot_label->setAlignment(Qt::AlignCenter);
 
-    m_color = "color: black";
-    m_back_button->setStyleSheet(m_color);
-    m_color = "background-color: gray";
-    m_back_button->setStyleSheet(m_color);
-
-    m_rules_layout->addWidget(m_back_button);
-    m_rules_layout->addWidget(m_text_label);
-    m_rules_layout->addWidget(m_exit);
-
-    m_rules_window->setLayout(m_rules_layout);
-
-    setCentralWidget(m_rules_window);
+    m_slot_label->setFixedSize(900,1000);
 }
 
+void MainWindow::about_slot(){
+    m_slot_label->setText("I'm a Bachelor's degree student\n"
+                          "who want to get max point\n"
+                          "for this work");
+    m_font.setPointSize(25);
+    m_slot_label->setFont(m_font);
+    m_slot_label->setAlignment(Qt::AlignCenter);
+
+    m_slot_label->setFixedSize(900,1000);
+}
 
 void MainWindow::exit_slot(){
     close();
@@ -155,21 +148,15 @@ void MainWindow::exit_slot(){
 
 MainWindow::~MainWindow()
 {
-    delete m_layout;
-    delete m_buttos_layout;
-    delete m_URL_label;
+    delete m_all_layout;
+    delete m_menu_layout;
     delete m_wellcome_label;
-    delete m_window;
+    delete m_URL_label;
+    delete m_wellcome_layout;
     delete m_play;
     delete m_rules;
     delete m_about_me;
     delete m_exit;
-    delete m_wellcome_layout;
-    delete m_rules_window;
-    delete m_back_button;
-    delete m_text_label;
-    delete m_rules_layout;
-    delete m_about_window;
-    delete m_about_layout;
+    delete m_buttos_layout;
+    delete m_window;
 }
-
--- UI/mainwindow.h
+++ UI/mainwindow.h
@@ -2,13 +2,9 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QHBoxLayout>
 #include <QLabel>
 #include <QPushButton>
-#include <QVBoxLayout>
-#include <QtWidgets>
-#include <QString>
-
-
 
 class MainWindow : public QMainWindow
 {
@@ -23,27 +19,26 @@ public slots:
     void rule_slot();
     void about_slot();
     void exit_slot();
-    void back_slot();
 
 private:
+    QLabel *m_slot_label = new QLabel();
+    QHBoxLayout *m_all_layout = new QHBoxLayout();
+    QVBoxLayout *m_menu_layout = new QVBoxLayout();
     QLabel *m_wellcome_label = new QLabel();
     QLabel *m_URL_label = new QLabel();
-    QWidget *m_window = new QWidget();
+    QVBoxLayout *m_wellcome_layout = new QVBoxLayout();
     QPushButton *m_play = new QPushButton();
     QPushButton *m_rules = new QPushButton();
     QPushButton *m_about_me = new QPushButton();
     QPushButton *m_exit = new QPushButton();
-    QVBoxLayout *m_buttos_layout = new QVBoxLayout;
-    QHBoxLayout *m_wellcome_layout = new QHBoxLayout;
-    QVBoxLayout *m_layout = new QVBoxLayout;
+    QVBoxLayout *m_buttos_layout = new QVBoxLayout();
+    QWidget *m_window = new QWidget();
     QFont m_font;
     QString m_color;
-    QWidget *m_rules_window = new QWidget();
-    QPushButton *m_back_button = new QPushButton();
-    QLabel *m_text_label = new QLabel();
-    QVBoxLayout *m_rules_layout = new QVBoxLayout();
-    QWidget *m_about_window = new QWidget();
-    QVBoxLayout *m_about_layout = new QVBoxLayout();
+
+    QPushButton *m_check_button = new QPushButton();
+    QGridLayout *m_board_grid = new QGridLayout();
+    QVBoxLayout *m_play_slot_layout = new QVBoxLayout();
 };
 
 #endif // MAINWINDOW_H
